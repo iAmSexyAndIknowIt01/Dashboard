9648697d1558d614f9f66c12eb8b264fdd00960b
teke <tekeb98@gmail.com>
2025-02-11 21:59:30 +0900

    DBに請求書を登録

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,45 @@
 'use server';
 
+import { date, string, z } from 'zod';
+import { db } from '@vercel/postgres';
+import { InvoicesTable } from './definitions';
+import { revalidatePath } from 'next/cache';
+// import postgres from 'postgres';
+
+
+// const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
+
+const invoice = await db.connect();
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({id: true, date: true});
+
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    };
-    console.log(rawFormData);
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    console.log(typeof customerId, ":" + customerId);
+    console.log(typeof amountInCents, ":" + amountInCents);
+    console.log(typeof status, ":" + status);
+    console.log(typeof date, ":" + date);
+
+    await invoice.sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date});
+    `;
+
+    revalidatePath('/dashboard/invoices');
+
 }
