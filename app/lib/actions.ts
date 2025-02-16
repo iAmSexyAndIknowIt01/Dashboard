b745bf5a97f0013010da6022b895bb344d4edb06
teke <tekeb98@gmail.com>
2025-02-16 19:16:31 +0900

    Handling 404 errors with the notFound function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,11 @@
 'use server';
 
 import { date, string, z } from 'zod';
-import { db } from '@vercel/postgres';
+import { db, sql } from '@vercel/postgres';
 import { InvoicesTable } from './definitions';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
+import { constants } from 'buffer';
 // import postgres from 'postgres';
 
 
@@ -21,6 +22,35 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
+const UpdateInvoice = FormSchema.omit({id: true, date: true});
+
+export async function deleteInvoice(id: string){
+
+    throw new Error('Failed to Delete Invoice');
+
+    await invoice.sql`DELETE FROM invoices WHERE id = ${id}`;
+
+    revalidatePath('/dashboard/invoices');
+}
+
+export async function updateInvoice(id: string, formData: FormData){
+    const { customerId, amount, status } = UpdateInvoice.parse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    }); 
+
+    const amountInCents = amount * 100;
+
+    await invoice.sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+    `;
+
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+}
 
 export async function createInvoice(formData: FormData) {
     const { customerId, amount, status } = CreateInvoice.parse({
@@ -31,15 +61,14 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
-    console.log(typeof customerId, ":" + customerId);
-    console.log(typeof amountInCents, ":" + amountInCents);
-    console.log(typeof status, ":" + status);
-    console.log(typeof date, ":" + date);
-
-    await invoice.sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date});
-    `;
+    try{
+        await invoice.sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date});
+        `;
+    }catch(error){
+        console.log(error);
+    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
